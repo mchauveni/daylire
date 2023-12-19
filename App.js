c78691a529b5682f6cde9f58585b9736c15ac9c8
mchauveni <matheo.chaumet.venier@gmail.com>
2023-12-19 20:10:36 +0100

    First commit : 2 pages, some work, some doesnt

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import loufokerie from "./components/loufokerie";
+import { useEffect, useState } from 'react';
+import ApiHandler from './apihandler';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { NativeRouter, Route, Routes, Link } from "react-router-native";
+import Loufokerie from './components/loufokerie';
+
+const Home = () => {
+	const [loufokeries, setLoufokeries] = useState(null);
+
+	const fetchLoufokeries = async () => {
+		setLoufokeries(await ApiHandler.fetchAllLoufokeries())
+	}
+
+	useEffect(() => {
+		fetchLoufokeries();
+	}, [])
+
+	return (
+		<>
+			<Text>Toutes les loufokeriesss !!</Text>
+			{
+				loufokeries ?
+					loufokeries.map((loufokerie) => (
+						<Link key={loufokerie.id} style={styles.loufokeriewrapper} to={"/" + loufokerie.id}>
+							<Text style={styles.loufokerietitle}>{loufokerie.titre_loufokerie}</Text>
+						</Link>
+					)) : (
+						<Text>Chargement...</Text>
+					)
+			}
+		</>
+	)
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NativeRouter>
+			<View style={styles.container}>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/:loufokId" element={<Loufokerie />} />
+				</Routes>
+			</View>
+		</NativeRouter>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		paddingTop: 32,
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'flex-start',
+	},
+	loufokeriewrapper: {
+		margin: 8,
+		display: "flex",
+		width: "90%",
+		backgroundColor: '#8531da',
+		padding: 16,
+		borderRadius: 8,
+	},
+	loufokerietitle: {
+		fontWeight: "600",
+		fontSize: 32,
+		color: "#fff",
+	}
 });
---
+++ apihandler.js
@@ -0,0 +1,54 @@
+class ApiHandler {
+    static async fetchAllLoufokeries() {
+        return await ApiHandler.get("https://mchauveni.alwaysdata.net/loufok_app/api/loufokeries");
+    }
+
+    static async fetchLoufokerie(id) {
+        return await ApiHandler.get(`https://mchauveni.alwaysdata.net/loufok_app/api/loufokerie/${id}`);
+    }
+
+    static async postLike(body) {
+        return await ApiHandler.post("https://mchauveni.alwaysdata.net/loufok_app/api/like", body);
+    }
+
+    static async get(url) {
+        const options = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+            }
+        };
+
+        try {
+            const response = await fetch(url, options);
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+
+    static async post(url, body) {
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+            },
+            body: body
+        };
+
+        try {
+            const response = await fetch(url, options);
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+}
+
+export default ApiHandler;
